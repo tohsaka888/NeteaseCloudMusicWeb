1cc1d8b7052ed6366fe2fd9a73b62ee8d4718aeb
tohsaka888 <156132264@qq.com>
2021-12-06 16:18:57 +0800

    主页配置

---
+++ src/pages/DjRadio.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DjRadio() {
+  return (
+    <div>
+      DjRadio
+    </div>
+  )
+}
---
+++ src/pages/Toplist.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Toplist() {
+  return (
+    <div>
+      Toplist
+    </div>
+  )
+}
