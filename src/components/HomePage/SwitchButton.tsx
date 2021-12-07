b7b11829b31530f9b6084b5fb2fef1558e24e817
tohsaka888 <156132264@qq.com>
2021-12-07 16:38:14 +0800

    新增切换按钮控制Ref实现轮播图切换

--- src/components/HomePage/SwitchButton.tsx
+++ src/components/HomePage/SwitchButton.tsx
@@ -1,14 +1,52 @@
-import React from 'react'
-import styled from 'styled-components'
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { BannerContext } from "../../context/HomePage/Context";
 
-const Container = styled.div`
-  
-`
+type Props = {
+  direction: string;
+};
 
-export default function SwitchButton() {
+const Container = styled.div<Props>`
+  position: absolute;
+  z-index: 1000;
+  height: 273px;
+  display: flex;
+  align-items: center;
+  left: ${({ direction }) => (direction === "left" ? "12vw" : "85vw")};
+  font-weight: 100;
+`;
+
+export default function SwitchButton({ direction }: Props) {
+  const props = useContext(BannerContext);
   return (
-    <div>
-      
-    </div>
-  )
+    <Container direction={direction}>
+      {direction === "left" && (
+        <LeftOutlined
+          onClick={() => {
+            props?.bannerRef.current?.prev();
+          }}
+          style={{
+            color: "white",
+            fontSize: "3rem",
+            fontWeight: 100,
+            cursor: "pointer",
+          }}
+        />
+      )}
+      {direction === "right" && (
+        <RightOutlined
+          onClick={() => {
+            props?.bannerRef.current?.next();
+          }}
+          style={{
+            color: "white",
+            fontSize: "3rem",
+            fontWeight: 100,
+            cursor: "pointer",
+          }}
+        />
+      )}
+    </Container>
+  );
 }
