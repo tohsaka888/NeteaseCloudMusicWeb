aed252f172b0343319496737f2eefb4da7f5a8af
tohsaka888 <156132264@qq.com>
2021-12-07 15:02:18 +0800

    更改主页布局

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,23 @@
+import { Layout } from "antd";
 import React from "react";
+import styled from "styled-components";
+import Banner from "../components/HomePage/Banner";
+import '../styles/HomePage.css'
+
+const Container = styled.div`
+  width: 64vw;
+  margin: 0 18vw;
+  background-color: white;
+  height: 100vh;
+  box-sizing: border-box;
+`;
 
 export default function HomePage() {
+  const { Content } = Layout;
   return (
-    <div>
-      HomePage
-    </div>
+    <Content className="content">
+      <Banner />
+      <Container>1111</Container>
+    </Content>
   );
 }
