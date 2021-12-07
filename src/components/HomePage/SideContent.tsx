2310b1bc9a89d04317fe057be9672b675348a748
tohsaka888 <156132264@qq.com>
2021-12-07 21:32:06 +0800

    侧边内容区

---
+++ src/components/HomePage/SideContent.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 16vw;
+  height: 100%;
+  border: 1px solid #cecece;
+`
+
+export default function SideContent() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
