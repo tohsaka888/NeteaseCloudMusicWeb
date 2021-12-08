e67def3cc3fa9c51a986a4e3afd7318b873414c1
tohsaka888 <156132264@qq.com>
2021-12-08 20:33:53 +0800

    捕获请求异常

--- src/request/HomePage/Banner.ts
+++ src/request/HomePage/Banner.ts
@@ -1,14 +1,20 @@
 import { BaseUrl } from "../BaseUrl";
+import { networkError } from "../Errors";
 
 const sendRequest = async () => {
-  const res = await fetch(`${BaseUrl}/banner`, {
-    method: "POST",
-    body: JSON.stringify({ type: 0 }),
-  });
-  const data = await res.json();
-  return data.code === 200
-    ? data.banners
-    : { errmsg: "请求失败,请检查网络配置" };
+  try {
+    const res = await fetch(`${BaseUrl}/banner`, {
+      method: "POST",
+      body: JSON.stringify({ type: 0 }),
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    });
+    const data = await res.json();
+    return data.code === 200 ? data.banners : networkError;
+  } catch (error) {
+    return networkError;
+  }
 };
 
 export { sendRequest };
