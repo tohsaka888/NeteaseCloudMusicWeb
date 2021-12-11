3cb3216629eac32df8bcaeda41957d39aee7706d
tohsaka888 <156132264@qq.com>
2021-12-11 14:11:37 +0800

    二维码登录逻辑修改

--- src/request/Header/Login.ts
+++ src/request/Header/Login.ts
@@ -1,4 +1,5 @@
-import { BaseUrl } from "../BaseUrl";
+// import { message } from "antd";
+import { BaseUrl, httpHeader } from "../BaseUrl";
 import { networkError } from "../Errors";
 
 // 先生产二维码Key
@@ -6,36 +7,38 @@ const createQRKey = async () => {
   try {
     const res = await fetch(`${BaseUrl}/login/qr/key`);
     const data = await res.json();
-    return data.code === 200 ? data : networkError;
+    return data.code === 200 ? data.data.unikey : networkError;
   } catch (error) {
     return networkError;
   }
 };
 
-// 通过key取生成二维码 使用SSE
 const createQRCode = async (key: string) => {
-  const res = await fetch(`${BaseUrl}/login/qr/create`, {
-    method: "POST",
-    body: JSON.stringify({ key: key }),
-  });
-  const data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch(`${BaseUrl}/login/qr/create`, {
+      method: "POST",
+      body: JSON.stringify({ key: key, qrimg: true }),
+      headers: httpHeader,
+    });
+    const data = await res.json();
+    return data.code ? data.data.qrimg : networkError;
+  } catch (error) {
+    return networkError;
+  }
 };
 
 // 获取二维码当前状态
 const checkQRStatus = async (key: string) => {
-  // const res = await fetch(`${BaseUrl}/login/qr/check?key=${key}`, {
-  //   method: "POST",
-  //   body: JSON.stringify({ key: key }),
+  const date = Date.now()
+  const res = await fetch(`${BaseUrl}/login/qr/check?key=${key}&timerstamp=${date}`);
+  const data = await res.json();
+  return data;
+  // const eventSource = new EventSource(`${BaseUrl}/login/qr/check?key=${key}`, {
+  //   withCredentials: true,
   // });
-  // const data = await res.json();
-  // console.log(data);
-  const eventSource = new EventSource(`${BaseUrl}/login/qr/check?key=${key}`, {
-    withCredentials: true,
-  });
-  eventSource.onmessage = function (e) {
-    console.log(e.data);
-  };
+  // eventSource.onmessage = function (e) {
+  //   console.log(e.data);
+  // };
 };
 
 export { createQRCode, createQRKey, checkQRStatus };
