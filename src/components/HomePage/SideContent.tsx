d7645e2ec94b23dd5d3cb901f6fdc4631ab13fbb
tohsaka888 <156132264@qq.com>
2021-12-12 16:20:08 +0800

    主页侧边内容区域

--- src/components/HomePage/SideContent.tsx
+++ src/components/HomePage/SideContent.tsx
@@ -1,15 +1,18 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import Login from "./Login";
+import TopArtists from "./TopArtists";
 
 const Container = styled.div`
   width: 16vw;
   height: 100%;
   border: 1px solid #cecece;
-`
+`;
 
 export default function SideContent() {
   return (
     <Container>
-      
+      <Login />
+      <TopArtists />
     </Container>
-  )
+  );
 }
