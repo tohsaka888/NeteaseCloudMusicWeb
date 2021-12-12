922142182e012ffeb03146c4e9ac746e483a785d
tohsaka888 <156132264@qq.com>
2021-12-12 12:40:15 +0800

    跨域携带cookie

--- src/request/Header/Login.ts
+++ src/request/Header/Login.ts
@@ -5,7 +5,10 @@ import { networkError } from "../Errors";
 // 先生产二维码Key
 const createQRKey = async () => {
   try {
-    const res = await fetch(`${BaseUrl}/login/qr/key`);
+    const res = await fetch(`${BaseUrl}/login/qr/key`, {
+      mode: "cors",
+      credentials: "include",
+    });
     const data = await res.json();
     return data.code === 200 ? data.data.unikey : networkError;
   } catch (error) {
@@ -19,6 +22,8 @@ const createQRCode = async (key: string) => {
       method: "POST",
       body: JSON.stringify({ key: key, qrimg: true }),
       headers: httpHeader,
+      mode: "cors",
+      credentials: "include",
     });
     const data = await res.json();
     return data.code ? data.data.qrimg : networkError;
@@ -29,8 +34,15 @@ const createQRCode = async (key: string) => {
 
 // 获取二维码当前状态
 const checkQRStatus = async (key: string) => {
-  const date = Date.now()
-  const res = await fetch(`${BaseUrl}/login/qr/check?key=${key}&timerstamp=${date}`);
+  const date = Date.now();
+  const res = await fetch(
+    `${BaseUrl}/login/qr/check?key=${key}&timerstamp=${date}`,
+    {
+      mode: "cors",
+      credentials: "include",
+      // headers: httpHeader,
+    }
+  );
   const data = await res.json();
   return data;
   // const eventSource = new EventSource(`${BaseUrl}/login/qr/check?key=${key}`, {
