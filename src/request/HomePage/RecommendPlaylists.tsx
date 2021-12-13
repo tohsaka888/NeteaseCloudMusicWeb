b069f09d98298b8f8151494a134b159086581d67
tohsaka888 <156132264@qq.com>
2021-12-13 10:18:28 +0800

    获取推荐歌单

---
+++ src/request/HomePage/RecommendPlaylists.tsx
@@ -0,0 +1,18 @@
+import { BaseUrl } from "../BaseUrl";
+import { networkError } from "../Errors";
+
+const getRecommendPlaylists = async () => {
+  try {
+    const res = await fetch(`${BaseUrl}/recommend/resource`, {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return networkError;
+  }
+};
+
+export { getRecommendPlaylists };
