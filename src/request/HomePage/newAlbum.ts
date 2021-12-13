cdf0f29c9f36603cd8c1332fa349f97246664662
tohsaka888 <156132264@qq.com>
2021-12-13 19:06:45 +0800

    新碟上架

---
+++ src/request/HomePage/newAlbum.ts
@@ -0,0 +1,28 @@
+import { BaseUrl } from "../BaseUrl";
+
+const getNewAlbum = async () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  let album = [];
+  try {
+    const res = await fetch(
+      `${BaseUrl}/top/album?offset=0&limit=10&year=${year}&month=${month}`,
+      {
+        method: "GET",
+        credentials: "include",
+      }
+    );
+    const data = await res.json();
+    if (data.code === 200) {
+      album.push(data.weekData.slice(0, 5));
+      album.push(data.weekData.slice(5, 10));
+    }
+    return { fail: false, album };
+  } catch (error) {
+    // return networkError;
+    console.log(error)
+  }
+};
+
+export { getNewAlbum };
