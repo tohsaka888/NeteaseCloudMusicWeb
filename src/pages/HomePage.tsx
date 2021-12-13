8560528127ed9bd637b145b75c711f72a4b50994
tohsaka888 <156132264@qq.com>
2021-12-13 10:18:14 +0800

    主页

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
 import { Layout } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Banner from "../components/HomePage/Banner";
 import MainContent from "../components/HomePage/MainContent";
 import SideContent from "../components/HomePage/SideContent";
+import { getLoginStatus } from "../request/Header/Login";
 import "../styles/HomePage.css";
 
 const Container = styled.div`
@@ -11,12 +12,25 @@ const Container = styled.div`
   margin: 0 18vw;
   display: flex;
   background-color: white;
-  height: 100vh;
   box-sizing: border-box;
 `;
 
 export default function HomePage() {
   const { Content } = Layout;
+  const [userData, setUserData] = useState<any>();
+  useEffect(() => {
+    const sendRequest = async () => {
+      const data = await getLoginStatus();
+      sessionStorage.setItem("userData", data);
+      setUserData(userData);
+    };
+    if (sessionStorage.getItem("userData")) {
+    } else {
+      sendRequest();
+      setUserData(userData);
+    }
+    console.log(sessionStorage.getItem("userData"));
+  }, [userData]);
   return (
     <Content className="content">
       <Banner />
