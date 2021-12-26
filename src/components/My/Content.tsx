7389fb36fa0c6d645b7406ef8d3e7ae4f2b1ae22
tohsaka888 <156132264@qq.com>
2021-12-26 12:11:22 +0800

    outlet加载动态路由

--- src/components/My/Content.tsx
+++ src/components/My/Content.tsx
@@ -1,6 +1,11 @@
 import { Layout } from "antd";
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function Content() {
-  return <Layout.Content></Layout.Content>;
+  return (
+    <Layout.Content>
+      <Outlet />
+    </Layout.Content>
+  );
 }
