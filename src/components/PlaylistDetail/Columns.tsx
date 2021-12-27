0eceef308a18fac60cf14f6126ef1a131f44b217
tohsaka888 <156132264@qq.com>
2021-12-27 21:37:57 +0800

    表格columns配置项

---
+++ src/components/PlaylistDetail/Columns.tsx
@@ -0,0 +1,75 @@
+import { PlayCircleOutlined } from "@ant-design/icons";
+import { ColumnsType } from "antd/lib/table";
+import moment from "moment";
+import styled from "styled-components";
+
+const Index = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: #333333;
+`;
+
+const TextEllpsis = styled.div`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
+export const columns: ColumnsType<any> = [
+  {
+    title: "",
+    dataIndex: "index",
+    key: "index",
+    width: "10%",
+    ellipsis: true,
+    render: (value, record, index) => {
+      return (
+        <Index>
+          <div style={{ color: "#333333" }}>{index + 1}</div>
+          <PlayCircleOutlined color="#333333" />
+        </Index>
+      );
+    },
+  },
+  {
+    title: "歌曲标题",
+    dataIndex: "name",
+    key: "name",
+    width: "40%",
+    ellipsis: true,
+  },
+  {
+    title: "时长",
+    dataIndex: "dt",
+    key: "dt",
+    width: "10%",
+    ellipsis: true,
+    render: (value) => (
+      <TextEllpsis>{moment(value).format("mm:ss")}</TextEllpsis>
+    ),
+  },
+  {
+    title: "歌手",
+    dataIndex: "ar",
+    key: "ar",
+    width: "20%",
+    ellipsis: true,
+    render: (value) => (
+      <TextEllpsis>
+        {value.length !== 0 &&
+          value.map((item: any) => {
+            return item.name;
+          })}
+      </TextEllpsis>
+    ),
+  },
+  {
+    title: "专辑",
+    dataIndex: "al",
+    key: "al",
+    width: "20%",
+    ellipsis: true,
+    render: (value) => <TextEllpsis>{value.name}</TextEllpsis>,
+  },
+];
