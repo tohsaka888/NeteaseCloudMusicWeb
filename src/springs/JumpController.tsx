0d111d5503224d058ac93518d0ca7e2af4bdffdc
tohsaka888 <156132264@qq.com>
2021-12-28 23:18:31 +0800

    播放器跳动动画

---
+++ src/springs/JumpController.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+import { useSpring, animated, config } from "react-spring";
+
+type Props = {
+  children: ReactNode;
+  isShow?: boolean;
+};
+
+export default function JumpController({ children, isShow = false }: Props) {
+  const props = useSpring({
+    from: {
+      height: isShow ? "10px" : "60px",
+      overflow: "hidden",
+    },
+    to: {
+      height: isShow ? "60px" : "10px",
+    },
+    config: config.gentle,
+  });
+  return <animated.div style={props}>{children}</animated.div>;
+}
