7a8c6e14b378f2103cbb2268b7d0253c19d354cd
tohsaka888 <156132264@qq.com>
2021-12-29 14:37:27 +0800

    优化hook

--- src/hooks/useLoginStatus.tsx
+++ src/hooks/useLoginStatus.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
+import { networkError } from "../request/Errors";
 import { getLoginStatus } from "../request/Header/Login";
 
 export default function useLoginStatus() {
-  const [loginStatus, setLoginStatus] = useState<any>({code: 301});
+  const [loginStatus, setLoginStatus] = useState<any>({ code: 301 });
   useEffect(() => {
     const sendRequest = async () => {
-      const data = await getLoginStatus();
-      setLoginStatus(data.data);
+      try {
+        const data = await getLoginStatus();
+        setLoginStatus(data.data);
+      } catch (error) {
+        return networkError;
+      }
     };
     sendRequest();
   }, []);
