2b5d7b14f2e83f01dfad3f54a93216349437a827
tohsaka888 <156132264@qq.com>
2021-12-30 17:25:09 +0800

    歌曲详情页右侧Content

---
+++ src/components/SongDetail/RightPart.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 25%;
+  border-left: 1px solid #dfdfdf;
+`;
+
+export default function RightPart() {
+  return <Container></Container>;
+}
