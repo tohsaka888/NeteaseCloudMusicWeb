a45deceb0618b3b831f743cc31cfddfb0dc8cafd
tohsaka888 <156132264@qq.com>
2021-12-31 15:07:39 +0800

    歌曲详情页右侧列表

--- src/components/SongDetail/RightPart.tsx
+++ src/components/SongDetail/RightPart.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import Download from "./Download";
+import SemiPlaylists from "./SemiPlaylists";
+import SemiSongs from "./SemiSongs";
 
 const Container = styled.div`
   width: 25%;
   border-left: 1px solid #dfdfdf;
+  display: flex;
+  flex-direction: column;
 `;
 
 export default function RightPart() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <SemiPlaylists />
+      <SemiSongs />
+      <Download />
+    </Container>
+  );
 }
