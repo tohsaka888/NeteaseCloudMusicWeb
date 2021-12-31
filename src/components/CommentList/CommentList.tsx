4d4d4b51453a10e6e5899bb83e4b88d50954fbaa
tohsaka888 <156132264@qq.com>
2021-12-31 15:06:52 +0800

    增加评论区

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,8 +1,10 @@
 import { LikeOutlined } from "@ant-design/icons";
-import { Comment, Divider, List } from "antd";
-import React from "react";
-import { useParams } from "react-router-dom";
+import { Textarea } from "@chakra-ui/react";
+import { Button, Comment, Divider, List } from "antd";
+import React, { useContext } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
+import { LoginContext } from "../../context/Context";
 import useHttpRequest from "../../hooks/useHttpRequest";
 import ListHeader from "./ListHeader";
 
@@ -34,12 +36,28 @@ const LikedCount = styled.div`
   margin-left: 4px;
 `;
 
+const Title = styled.div`
+  padding: 8px 0px;
+  font-size: 20px;
+  border-bottom: 1px solid #cecece;
+  margin-bottom: 16px;
+  font-weight: bold;
+  margin-top: 24px;
+`;
+
+const UserName = styled.div`
+  font-size: 14px;
+  color: #000;
+  text-shadow: 1px 1px 1px #cecece;
+  font-weight: bold;
+`;
+
 const Action = ({ item }: { item: any }) => {
   return (
     <Flex>
       <Reply>回复</Reply>
       <LikeOutlined style={{ color: "#0c73c2" }} />
-      <LikedCount>{item.likedCount}</LikedCount>
+      <LikedCount>{item.likedCount || 0}</LikedCount>
     </Flex>
   );
 };
@@ -50,7 +68,7 @@ const MusicComment = ({ item, index }: Props) => {
       <Comment
         content={item.content}
         avatar={item.user.avatarUrl}
-        author={item.user.nickname}
+        author={<UserName>{item.user.nickname}</UserName>}
         datetime={item.timeStr}
         actions={[<Action item={item} />]}
       />
@@ -58,6 +76,7 @@ const MusicComment = ({ item, index }: Props) => {
         item.beReplied.map((replyItem: any, index: number) => {
           return (
             <Comment
+              key={index}
               style={{
                 background: "#f4f4f4",
                 border: "1px solid #dedede",
@@ -79,14 +98,31 @@ const MusicComment = ({ item, index }: Props) => {
 
 export default function CommentList() {
   const params = useParams();
+  const location = useLocation();
+  const { loginStatus } = useContext(LoginContext);
   const data = useHttpRequest({
-    api: "/comment/music",
+    api:
+      location.pathname.includes("playlist") || location.pathname.includes("my")
+        ? "/comment/playlist"
+        : "/comment/music",
     method: "GET",
     credentials: "include",
     requestData: JSON.stringify({ id: params.id }),
   });
   return (
     <Container>
+      <Title>评论区</Title>
+      <Comment
+        content={<Textarea rows={4} />}
+        avatar={(loginStatus.profile && loginStatus.profile.avatarUrl) || ""}
+        author={
+          <UserName>
+            {(loginStatus.profile && loginStatus.profile.nickname) ||
+              "游客账户"}
+          </UserName>
+        }
+        actions={[<Button type="primary">发送评论</Button>]}
+      />
       <List
         style={{ marginTop: "24px" }}
         header={
@@ -96,7 +132,7 @@ export default function CommentList() {
           />
         }
         dataSource={data.hotComments}
-        pagination={{ pageSize: 5 }}
+        pagination={{ pageSize: 5, size: "small" }}
         renderItem={(item, index) => <MusicComment item={item} index={index} />}
       />
 
@@ -104,7 +140,7 @@ export default function CommentList() {
         style={{ marginTop: "24px" }}
         header={<ListHeader total={data.comments && data.comments.length} />}
         dataSource={data.comments}
-        pagination={{ pageSize: 10 }}
+        pagination={{ pageSize: 10, size: "small" }}
         renderItem={(item, index) => <MusicComment item={item} index={index} />}
       />
     </Container>
