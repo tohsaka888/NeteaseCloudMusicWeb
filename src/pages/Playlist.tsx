41effe348f6e0235f640beddd6f43f4631cb8b95
tohsaka888 <156132264@qq.com>
2022-01-02 16:44:37 +0800

    歌单页

--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Content from "../components/playlist/Content";
+import Header from "../components/playlist/Header";
+
+const Container = styled.div`
+  margin: 0px auto;
+  height: 64vw;
+  min-width: 950px;
+  padding: 36px 24px;
+  background-color: white;
+  border-left: 1px solid #cecece;
+  border-right: 1px solid #cecece;
+`;
 
 export default function Playlist() {
   return (
-    <div>
-      playlist
-    </div>
-  )
+    <Container>
+      <Header />
+      <Content />
+    </Container>
+  );
 }
