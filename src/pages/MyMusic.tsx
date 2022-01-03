45da2793c4e09a98334cf7f0a238f87e8b5762c1
tohsaka888 <156132264@qq.com>
2022-01-03 13:28:50 +0800

    我的音乐页面增加未登录显示

--- src/pages/MyMusic.tsx
+++ src/pages/MyMusic.tsx
@@ -1,14 +1,47 @@
 import { Layout } from "antd";
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
 import Content from "../components/My/Content";
 import Sider from "../components/My/Sider";
-import "../styles/MyMusic.css"
+import { LoginContext, VisibleContext } from "../context/Context";
+import "../styles/MyMusic.css";
+
+const Container = styled.div`
+  height: 82vh;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginImage = styled.div`
+  background: url("https://s2.music.126.net/style/web2/img/mymusic.png?b88819140383f3852a929febcaf805ac");
+  width: 807px;
+  height: 268px;
+  /* background-position: 0 104px; */
+  padding-top: 104px;
+  cursor: pointer;
+`;
 
 export default function MyMusic() {
+  const { loginStatus } = useContext(LoginContext);
+  const props = useContext(VisibleContext);
   return (
     <Layout className="container">
-      <Sider />
-      <Content />
+      {loginStatus.profile ? (
+        <>
+          <Sider />
+          <Content />
+        </>
+      ) : (
+        <Container>
+          <LoginImage
+            onClick={() => {
+              props?.setVisible(true);
+            }}
+          />
+        </Container>
+      )}
     </Layout>
   );
 }
