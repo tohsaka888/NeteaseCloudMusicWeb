81baacb10b6ae26acdc970aa551a11bf8e7d66ec
tohsaka888 <156132264@qq.com>
2022-01-03 13:28:23 +0800

    除了主页不显示底部导航

--- src/components/Header/HeaderBottom.tsx
+++ src/components/Header/HeaderBottom.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import "../../styles/Header.css";
 
@@ -7,10 +7,10 @@ type Props = {
   defaultSelectedKey: string;
 };
 
-const Container = styled.div`
+const Container = styled.div<{ show: boolean }>`
   background-color: #c20c0c;
   border-bottom: 1px solid #a40011;
-  height: 35px;
+  height: ${({ show }) => (show ? "35px" : "5px")};
   display: flex;
   align-items: center;
   justify-content: center;
@@ -52,68 +52,71 @@ const Item = styled.div<ItemProps>`
 export default function HeaderBottom({ defaultSelectedKey }: Props) {
   const [selectedKey, setSelectKey] = useState<string>(defaultSelectedKey);
   const navigate = useNavigate();
+  const location = useLocation();
   const moveTo = (route: string) => {
     navigate(route);
     setSelectKey(route);
   };
   return (
-    <Container>
-      <Menu>
-        <Item
-          selectedKey={selectedKey}
-          name="/"
-          onClick={() => {
-            moveTo("/");
-          }}
-        >
-          推荐
-        </Item>
-        <Item
-          selectedKey={selectedKey}
-          name="discover/toplist"
-          onClick={() => {
-            moveTo("discover/toplist");
-          }}
-        >
-          排行榜
-        </Item>
-        <Item
-          selectedKey={selectedKey}
-          name="discover/playlist"
-          onClick={() => {
-            moveTo("discover/playlist");
-          }}
-        >
-          歌单
-        </Item>
-        <Item
-          selectedKey={selectedKey}
-          name="discover/djradio"
-          onClick={() => {
-            moveTo("discover/djradio");
-          }}
-        >
-          主播电台
-        </Item>
-        <Item
-          selectedKey={selectedKey}
-          name="discover/artist"
-          onClick={() => {
-            moveTo("discover/artist");
-          }}
-        >
-          歌手
-        </Item>
-        <Item
-          selectedKey={selectedKey}
-          name="discover/album"
-          onClick={() => {
-            moveTo("discover/album");
-          }}
-        >
-          新碟上架
-        </Item>
-      </Menu>
+    <Container show={location.pathname === "/"}>
+      {location.pathname === "/" && (
+        <Menu>
+          <Item
+            selectedKey={selectedKey}
+            name="/"
+            onClick={() => {
+              moveTo("/");
+            }}
+          >
+            推荐
+          </Item>
+          <Item
+            selectedKey={selectedKey}
+            name="discover/toplist"
+            onClick={() => {
+              moveTo("discover/toplist");
+            }}
+          >
+            排行榜
+          </Item>
+          <Item
+            selectedKey={selectedKey}
+            name="discover/playlist"
+            onClick={() => {
+              moveTo("discover/playlist");
+            }}
+          >
+            歌单
+          </Item>
+          <Item
+            selectedKey={selectedKey}
+            name="discover/djradio"
+            onClick={() => {
+              moveTo("discover/djradio");
+            }}
+          >
+            主播电台
+          </Item>
+          <Item
+            selectedKey={selectedKey}
+            name="discover/artist"
+            onClick={() => {
+              moveTo("discover/artist");
+            }}
+          >
+            歌手
+          </Item>
+          <Item
+            selectedKey={selectedKey}
+            name="discover/album"
+            onClick={() => {
+              moveTo("discover/album");
+            }}
+          >
+            新碟上架
+          </Item>
+        </Menu>
+      )}
     </Container>
   );
 }
