aaebb2f0fd85f54e321ce1e9e2fc157460531b46
tohsaka888 <156132264@qq.com>
2022-01-04 12:10:18 +0800

    喜欢这个歌单的人

---
+++ src/components/PlaylistDetail/Subscribers.tsx
@@ -0,0 +1,47 @@
+import { Avatar, Col, Row } from "antd";
+import React from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import useHttpRequest from "../../hooks/useHttpRequest";
+import LoadingArea from "../HomePage/LoadingArea";
+
+export const Header = styled.div`
+  padding: 5px 0px;
+  border-bottom: 1px solid #cecece;
+`;
+
+export default function Subscribers() {
+  const params = useParams();
+  const data = useHttpRequest({
+    api: "/playlist/subscribers",
+    method: "GET",
+    credentials: "include",
+    requestData: JSON.stringify({ id: params.id }),
+  });
+  return (
+    <>
+      <Header>喜欢这个歌单的人</Header>
+      <Row>
+        {data.subscribers ? (
+          data.subscribers.map((item: any, index: number) => {
+            return (
+              <Col
+                span={6}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginTop: "8px",
+                }}
+              >
+                <Avatar src={item.avatarUrl} size={"large"} />
+              </Col>
+            );
+          })
+        ) : (
+          <LoadingArea height="20vh" />
+        )}
+      </Row>
+    </>
+  );
+}
