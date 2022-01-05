76b401bea1e1442ef5947b7021a29bfc5c998ec1
tohsaka888 <156132264@qq.com>
2022-01-05 17:13:09 +0800

    朋友界面

--- src/pages/Friend.tsx
+++ src/pages/Friend.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useContext } from "react";
+import LoginImage from "../components/Friend/LoginImage";
+import TrendsList from "../components/Friend/TrendsList";
+import { LoginContext } from "../context/Context";
 
 export default function Friend() {
-  return (
-    <div>
-      Friend
-    </div>
-  )
+  const { loginStatus } = useContext(LoginContext);
+  return <>{loginStatus.profile ? <TrendsList /> : <LoginImage />}</>;
 }
