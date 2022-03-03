73eb065d3a0c2290e8d12122ef62287c69aac7c1
tohsaka888 <156132264@qq.com>
2022-03-03 15:48:12 +0800

    feat: 增加搜索

--- src/components/SearchModal/index.tsx
+++ src/components/SearchModal/index.tsx
@@ -1,7 +1,7 @@
+import { Box, Flex } from "@chakra-ui/react";
 import { Popover } from "antd";
-import React, { ReactNode, useContext, useEffect } from "react";
+import React, { ReactNode, useContext, useEffect, useState } from "react";
 import styled from "styled-components";
-import { useFetch } from "usehooks-ts";
 import { SearchContext } from "../../context/Search/SearchContext";
 import { BaseUrl, httpHeader } from "../../request/BaseUrl";
 
@@ -9,20 +9,129 @@ type Props = {
   children: ReactNode;
 };
 
+type ResultType = {
+  albums: any[];
+  artists: any[];
+  songs: any[];
+  playlists: any[];
+};
+
 const SearchTitle = styled.div`
   font-size: 12px;
   color: #666666;
 `;
 
+const Ellipsis = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 180px;
+  padding-left: 8px;
+  margin-bottom: 4px;
+  cursor: pointer;
+  :hover {
+    background-color: #f2f2fc;
+  }
+`;
+
 const SearchResult = () => {
   const searchProps = useContext(SearchContext);
-  const { data, error } = useFetch<any>(`${BaseUrl}/search`, {
-    method: "POST",
-    headers: httpHeader,
-    body: JSON.stringify({ keywords: searchProps?.searchValue }),
-    credentials: "include",
+  const [result, setResult] = useState<ResultType>({
+    albums: [],
+    artists: [],
+    songs: [],
+    playlists: [],
   });
-  return <></>;
+  useEffect(() => {
+    if (searchProps?.searchValue) {
+      const sendRequest = async () => {
+        const res = await fetch(
+          `${BaseUrl}/search/suggest?keywords=${searchProps.searchValue}&type=web`,
+          {
+            method: "GET",
+            headers: httpHeader,
+            credentials: "include",
+            mode: "cors",
+          }
+        );
+        const data = await res.json();
+        setResult(data.result);
+      };
+      sendRequest();
+    }
+  }, [searchProps?.searchValue]);
+  return (
+    <Flex w={["15vw"]} flexDir="column">
+      <Flex justify={"center"}>
+        <Flex flex={1} justify="flex-end" mr={"8px"}>
+          音乐
+        </Flex>
+        <Box borderLeft="1px solid #DDDDDD" flex={3}>
+          {result && result.songs ? (
+            result.songs.map((song, index) => (
+              <Ellipsis key={index}>{song.name}</Ellipsis>
+            ))
+          ) : (
+            <Ellipsis>暂无结果</Ellipsis>
+          )}
+        </Box>
+      </Flex>
+      <Flex justify={"center"}>
+        <Flex flex={1} justify="flex-end" mr={"8px"}>
+          歌手
+        </Flex>
+        <Box
+          borderLeft="1px solid #DDDDDD"
+          borderTop={"1px solid #DDDDDD"}
+          flex={3}
+        >
+          {result && result.artists ? (
+            result.artists.map((artist, index) => (
+              <Ellipsis key={index}>{artist.name}</Ellipsis>
+            ))
+          ) : (
+            <Ellipsis>暂无结果</Ellipsis>
+          )}
+        </Box>
+      </Flex>
+      <Flex justify={"center"}>
+        <Flex flex={1} justify="flex-end" mr={"8px"}>
+          歌单
+        </Flex>
+        <Box
+          borderLeft="1px solid #DDDDDD"
+          borderTop={"1px solid #DDDDDD"}
+          flex={3}
+        >
+          {result && result.playlists ? (
+            result.playlists.map((playlist, index) => (
+              <Ellipsis key={index}>{playlist.name}</Ellipsis>
+            ))
+          ) : (
+            <Ellipsis>暂无结果</Ellipsis>
+          )}
+        </Box>
+      </Flex>
+      <Flex justify={"center"}>
+        <Flex flex={1} justify="flex-end" mr={"8px"}>
+          专辑
+        </Flex>
+        <Box
+          borderLeft="1px solid #DDDDDD"
+          borderTop={"1px solid #DDDDDD"}
+          flex={3}
+        >
+          {result && result.albums ? (
+            result.albums.map((album, index) => (
+              <Ellipsis key={index}>{album.name}</Ellipsis>
+            ))
+          ) : (
+            <Ellipsis>暂无结果</Ellipsis>
+          )}
+        </Box>
+      </Flex>
+    </Flex>
+  );
 };
 
 function SearchModal({ children }: Props) {
@@ -30,7 +139,8 @@ function SearchModal({ children }: Props) {
   return (
     <Popover
       content={SearchResult}
-      visible={!!searchProps?.searchValue}
+      // visible={!!searchProps?.searchValue}
+      trigger={"focus"}
       placement={"bottomLeft"}
       title={
         <SearchTitle>搜"{searchProps?.searchValue}"相关的用户</SearchTitle>
