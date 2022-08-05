d7665ada2c31a8854a221ee3a9b7986df5fad2c5
tohsaka888 <156132264@qq.com>
2022-08-05 13:56:15 +0800

    🐳 chore: 去除无用依赖

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,10 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-08-04 14:47:21
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-08-05 13:43:30
+ * @Description: 请填写简介
+ */
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
